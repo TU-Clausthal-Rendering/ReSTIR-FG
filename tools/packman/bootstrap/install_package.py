e7b8acf0c14696f2363daa8754216482f1a27072
SirKero <sirkero@gmx.de>
2025-10-13 11:38:27 +0200

    Update Packman

--- tools/packman/bootstrap/install_package.py
+++ tools/packman/bootstrap/install_package.py
@@ -142,7 +142,7 @@ def generate_sha256_for_file(file_path: Union[str, os.PathLike]) -> str:
 
 
 def install_common_module(package_path, install_path):
-    COMMON_SHA256 = "ef974608cf903f39dbd3f22b3e26c15a1dd21f6e71d05c1510dcf423128cd7a4"
+    COMMON_SHA256 = "0900e096a55934b4c2ae885e483e9cd530fd7e14966a343c982c918d9ca2a5f5"
     package_sha256 = generate_sha256_for_file(package_path)
     if package_sha256 != COMMON_SHA256:
         raise RuntimeError(
